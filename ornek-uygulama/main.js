82baac5341a3c3cb4880d88b960314f90f67400b
muhammed-ogz <muhammetnabioguz@gmail.com>
2024-09-05 11:26:42 +0300

    sub-window and menu bar creation was done.

--- ornek-uygulama/main.js
+++ ornek-uygulama/main.js
@@ -1,22 +1,35 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 const isMac = process.platform == "darwin"; //mac işletim sistemleri için
-const isDev = process.env.MODE !== 'yayin';
+const isDev = process.env.MODE !== "yayin";
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: isDev ? 1200:800,
-    height: isDev ? 1000:650,
+    width: isDev ? 1200 : 800,
+    height: isDev ? 1000 : 650,
   });
 
   win.loadFile("./renderer/index.html");
-  if(isDev){
+  if (isDev) {
     win.webContents.openDevTools();
   }
 };
 
+const hakkimizdaWindow = () => {
+  const win2 = new BrowserWindow({
+    width: 500,
+    height: 500,
+    title : 'Biz Kimiz ?'
+  });
+
+  win2.loadFile("./renderer/hakkimizda.html");
+};
+
 app.whenReady().then(() => {
   createWindow();
+
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -24,6 +37,29 @@ app.whenReady().then(() => {
     }
   });
 });
+const menu = [
+  {
+    label: app.name,
+    submenu: [
+      {
+        label: "Hakkımızda",
+        click: () => {
+          hakkimizdaWindow();
+        },
+      },
+    ],
+  },
+  {
+    label: "Dosya",
+    submenu: [
+      {
+        label: "Çıkış",
+        click: () => app.quit(),
+        accelerator: "CmdOrCtrl+W",
+      },
+    ],
+  },
+];
 
 app.on("window-all-closed", () => {
   if (!isMac) {
